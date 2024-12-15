ddc1cb2afc832255813533334946572223ae2aa5
Yaroslav Bondar <freemannight@ukr.net>
2024-12-15 18:52:35 +0100

    fix pages

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
-
+  <div class="background-container" :style="backgroundStyle">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-
+  data() {
+    return {
+      backgroundUrl: require('@/assets/main_background_office.jpg')
+    };
+  },
+  computed: {
+    backgroundStyle() {
+      return {
+        backgroundImage: `url(${this.backgroundUrl})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+      };
+    }
   }
-}
+};
 </script>
 
-<style>
-
+<style scoped>
+.background-container {
+  width: 100%;
+  height: 400px;
+}
 </style>
